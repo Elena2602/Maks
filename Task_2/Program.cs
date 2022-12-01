53700042418744cc315c9a03199b8c270cce8c08
Elena2602 <118214409+Elena2602@users.noreply.github.com>
2022-12-01 22:16:02 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double b = a;
+if (b / 2 - a /2 == 0)
+
+Console.WriteLine("Четное");
+  else
+  Console.WriteLine("нечетное");
+
+
+
