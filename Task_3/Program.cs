87456f455e1d8ee88466bcad281dde78a3eba984
Elena2602 <118214409+Elena2602@users.noreply.github.com>
2022-12-01 22:50:10 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a < n)
+ {
+double b = a;
+if (b / 2 - a /2 == 0)
+Console.WriteLine(a);
+a = a + 1;
+}
+
+
+
+
